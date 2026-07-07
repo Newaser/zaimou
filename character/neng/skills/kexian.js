f70f9608c88bb2098a6f1ec7ae26088fca10e59d
Newaser <newaser888@outlook.com>
2026-07-07 12:48:34 +0800

    修改了技能”凿辟“、”克险“

--- character/neng/skills/kexian.js
+++ character/neng/skills/kexian.js
@@ -1,43 +1,81 @@
 import { SkillData } from "../../../utils/import.js";
 import { lib, game, ui, get, ai, _status } from "../../../../../noname.js";
 
+const DESCRIPTIONS = [
+	`<b>${get.poptip("contract_skill")}</b>，当你受到伤害时，你可以防止之。若如此做，你减1点体力上限并加1点蓄力上限。`,
+	"<b>锁定技</b>，当你受到伤害时，你防止之。若如此做，你减1点体力上限并加1点蓄力上限。",
+];
+
 /**
- * 
  * @param {Player} player 
- * @param {number} [num=1] 
  */
-function gainMaxCharge(player, num) {
-	player.markSkill("charge");
+function gianMaxCharge(player) {
+	game.log(player, "的蓄力值上限+1");
+	player.storage.zm_kexian_maxChargePlus++;
+	if (player.hasSkill("zm_yanji"))
+		player.markSkill("zm_yanji");
 }
 
 export default new SkillData("zm_kexian|克险", {
-	description: "当你受到伤害时，你可以防止之。若如此做，你减1点体力上限并加1点蓄力上限。",
+	description: DESCRIPTIONS[0],
+	dynamicDescription(player, desc) {
+		if (player.storage.zm_kexian) {
+			return DESCRIPTIONS[1];
+		}
+	},
 	voices: [
-		"蜀地天险，岂挡我军去路！",
+		"蜀地天险，岂足挡我军去路！",
 		"今马革山已取，众将随我冲！",
 	],
+	texts: {
+		"(poptip)contract_skill|契定技":
+			"发动一次后，此技能契定技标签改为锁定技并删除技能描述中的“可以”。",
+	},
 	skill: {
-		chargeSkill: 0,
 		trigger: { player: "damageBegin4" },
-		check(event, player, triggername, indexedData) {
-			return player.isDamaged();
+		async cost(event, trigger, player) {
+			if (!player.storage.zm_kexian) {
+				event.result = await player.chooseBool({
+					prompt: get.prompt("zm_kexian"),
+					prompt2: get.skillInfoTranslation("zm_kexian"),
+					ai(event, _) {
+						return player.isDamaged();
+					},
+				}).forResult();
+			} else {
+				event.result = { bool: true };
+			}
 		},
 		async content(event, trigger, player) {
+			const skillName = event.name;
+			if (!player.storage[skillName]) {
+				const animateName = `${skillName}_animate`;
+				player.trySkillAnimate(animateName, animateName, true);
+				player.storage[skillName] = true;
+			}
 			trigger.cancel();
 			await player.loseMaxHp();
-			game.log(player, "的蓄力值上限+1");
-			player.addMark("zm_kexian", 1, false);
-			if (player.hasSkill("zm_yaoji"))
-				player.markSkill("zm_yanji");
+			gianMaxCharge(player);
 		},
-		mod: {
-			maxCharge(player, num) {
-				return num + player.countMark("zm_kexian");
+		group: "zm_kexian_maxChargePlus",
+		subSkill: {
+			animate: {
+				skillAnimation: true,
+				animationColor: "thunder",
+			},
+			maxChargePlus: {
+				init(player, skill) {
+					player.setStorage(skill, 0);
+				},
+				mod: {
+					maxCharge(player, num) {
+						return num + player.storage.zm_kexian_maxChargePlus;
+					},
+				},
 			},
 		},
 		ai: {
-			maixie: true,
-			maixie_defend: true,
+			combo: "zm_yanji",
 		},
 	},
 });
--- character/neng/skills/zaopi.js
+++ character/neng/skills/zaopi.js
@@ -1,60 +1,59 @@
-import * as util from "../../../utils/util.js";
 import { SkillData } from "../../../utils/import.js";
 import { lib, game, ui, get, ai, _status } from "../../../../../noname.js";
 
+/** @type {CardBaseUIData} */
+const mtgh = {
+	name: "dz_mantianguohai",
+	isCard: true,
+	storage: { zm_zaopi: true },
+};
+
 export default new SkillData("zm_zaopi|凿辟", {
-	description: "准备阶段，你可以失去1点体力并视为使用一张【瞒天过海】。此后你与其他角色的距离-1。",
+	description:
+		"每回合限一次，你可以失去1点体力并视为使用一张" +
+		`${get.poptip("dz_mantianguohai")}。此后你与其他角色距离-1。`,
 	voices: [
 		"凿山开险，破蜀建功！",
 		"明战于剑阁，暗渡于阴平！",
 	],
 	skill: {
-		trigger: { player: "phaseZhunbeiBegin" },
-		filter(event, player, name, indexedData) {
-			return player.hp > 0;
-		},
-		check(event, player, triggername, indexedData) {
-			return player.hp > 1 &&
-				player.getUseValue("dz_mantianguohai") > 0;
-		},
-		async content(event, trigger, player) {
+		enable: "chooseToUse",
+		usable: 1,
+		viewAs: mtgh,
+
+		filterCard: (card, player) => false,
+		selectCard: -1,
+
+		async precontent(event, trigger, player) {
 			await player.loseHp();
-			const mtgh = { name: "dz_mantianguohai", isCard: true };
-			if (player.hasUseTarget(mtgh)) {
-				await util.chooseToViewAs(player, {
-					viewAs: mtgh,
-
-					forced: true,
-					prompt: "视为使用一张【瞒天过海】",
-
-					filterCard: (card, player) => false,
-					selectCard: -1,
-
-					filterTarget(card, player, target) {
-						return player.canUse("dz_mantianguohai", target);
-					},
-					selectTarget: [1, 2],
-
-					ai1(card) {
-						return -get.value(card);
-					},
-					ai2(target) {
-						return get.effect(target, mtgh, player, player);
-					},
-				});
-			}
-			player.addMark("zm_zaopi_unlimited");
 		},
-		group: "zm_zaopi_unlimited",
+		onuse(result, player) {
+			player
+				.when("useCardAfter")
+				.filter((event, player, name, indexedData) => {
+					return event.card.storage?.zm_zaopi === true;
+				})
+				.step(async (event, trigger, player) => {
+					player.addMark("zm_zaopi_distance");
+				});
+		},
+		group: "zm_zaopi_distance",
 		subSkill: {
-			unlimited: {
+			distance: {
 				intro: {
 					content: "你计算与其他角色的距离-#",
 				},
 				mod: {
 					globalFrom(from, to, current) {
-						return current - from.countMark("zm_zaopi_unlimited");
+						return current - from.countMark("zm_zaopi_distance");
 					},
+				},
+			},
+		},
+		ai: {
+			result: {
+				player(player, target, card) {
+					return player.hp > 1;
 				},
 			},
 		},
